54356b16abad81d5b1b299e1354d4e072458714e
Bluefin605 <bluefin605@gmail.com>
2026-05-22 08:01:37 +1200

    feat(angular): add Angular Material with default theme

--- frontend-angular/src/app/app.ts
+++ frontend-angular/src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatToolbarModule, MatButtonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('frontend-angular');
-}
+export class App {}
