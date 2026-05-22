c475d31398f415bff6abeabb401f7e63f55127d8
Bluefin605 <bluefin605@gmail.com>
2026-05-23 11:40:47 +1200

    chore(angular): add @angular/pwa baseline (service worker + manifest)

--- frontend-angular/src/app/app.config.ts
+++ frontend-angular/src/app/app.config.ts
@@ -1,4 +1,9 @@
-import { ErrorHandler, type ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ErrorHandler,
+  type ApplicationConfig,
+  provideZonelessChangeDetection,
+  isDevMode,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -7,6 +12,7 @@ import { routes } from './app.routes';
 import { authInterceptor } from './core/auth/auth-interceptor';
 import { errorInterceptor } from './core/api/error-interceptor';
 import { GlobalErrorHandler } from './core/error/global-error-handler';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +21,9 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
     provideAnimationsAsync(),
     { provide: ErrorHandler, useClass: GlobalErrorHandler },
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
